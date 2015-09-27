f4f16072780a08a142980315349149abb4c3444a
Erik Kynast <erikkynast@gmail.com>
2015-09-27 13:47:58 +0200

    bg image showing

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,25 +7,11 @@ angular.module('nightlifeApp')
 
     $scope.getData = function() {
 
+
       // only try to get data if the input is at least 3 chars long
       if($scope.testData.length >= 3) {
         console.log($scope.testData);
       }
+
     }
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
   });
